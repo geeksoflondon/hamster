8df4d3bd17b4751b83925b11936743d4ef204712
Kevin Prince <nextgengames@gmail.com>
2013-04-08 21:53:38 +0100

    adding diet submodule

---
+++ app/models/attendee/diet.rb
@@ -0,0 +1,14 @@
+class Attendee
+  class Diet
+    EVERYTHING  = 1.freeze
+    VEGETARIAN  = 2.freeze
+    VEGAN       = 3.freeze
+    NUT_ALLERGY = 4.freeze
+    LACTO_FREE  = 5.freeze
+    GLUTEN_FREE = 6.freeze
+    SHELLFISH   = 7.freeze
+    OTHER       = 8.freeze
+
+    SIZES = (1..8).freeze
+  end
+end
