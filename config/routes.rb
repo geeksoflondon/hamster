615eded267759b8448810fb45cdced22a143496c
Kevin Prince <nextgengames@gmail.com>
2013-05-17 18:46:38 +0100

    Adding newlines

--- config/routes.rb
+++ config/routes.rb
@@ -63,5 +63,3 @@ Hamster::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
-
-
