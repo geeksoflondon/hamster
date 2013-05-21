4d07e9312250a9ca4d998e54e6e71dc81a26643f
Kevin Prince <nextgengames@gmail.com>
2013-05-21 21:41:54 +0100

    completing implementation of the wristband class

--- spec/models/wristband_spec.rb
+++ spec/models/wristband_spec.rb
@@ -2,53 +2,39 @@ require 'spec_helper'
 
 describe Wristband do
   describe "#create" do
-    
     before(:each) do
       @ticket = a_saved Ticket
       @attendee = a_saved Attendee
     end
     
     it "should require a valid ticket" do
-      expect Wristband.create(@attendee, "123456").to raise_error
-      # Wristband.create(nil, "123456").should raise_error
-      #       Wristband.create(@ticket, "123456").should_not raise_error
-          end
+      expect { Wristband.create(@attendee, "123456") }.to raise_error
+      expect { Wristband.create(nil, "123456") }.to raise_error
+      expect { Wristband.create(@ticket, "123456") }.to_not raise_error
+    end
     
     it "should require the wristband id to be unused" do
-      
-    end
-    
-    it "should create a ticket interactable with a key of wristband" do
-    end
-    
-    it "should return true" do
+      Wristband.create(@ticket, "123456")
+      expect { Wristband.create(@ticket, "123456") }.to raise_error
+      expect { Wristband.create(@ticket, "654321") }.to_not raise_error
     end
   end
-  
-  describe "#swap" do
-    it "should require the original wristband to exist" do
-    end
-    
-    it "should require the new wristband to be unused" do
-    end
-    
-    it "should update the ticket interactable with the key of wristband with the value of the new id" do
-    end
-  end
-  
+
   describe "#find" do
-    it "should return false for a missing wristband id" do
+    before(:each) do
+      @ticket = a_saved Ticket
+      @attendee = a_saved Attendee
+      @wristband = Wristband.create(@ticket, "123456")
     end
     
     it "should return a ticket object for a valid wristband id" do
-    end
-  end
-  
-  describe "#exists?" do
-    it "should return false if the wristband id is not found" do
+      ticket = Wristband.find("123456")
+      ticket.should be_an_instance_of Ticket
+      ticket.id.should equal(@ticket.id)
     end
     
-    it "should return true if the wristband id is found" do
+    it "should raise an error for a missing wristband id" do
+      expect { Wristband.find("999999") }.to raise_error
     end
   end
 end
