69b7881780bb20f139e8f8803cbc390a382fe990
Kevin Prince <nextgengames@gmail.com>
2013-05-24 20:56:29 +0100

    Merging in wristband and completely rebuilding the zebra session control.

--- spec/controllers/zebra/sessions_spec.rb
+++ spec/controllers/zebra/sessions_spec.rb
@@ -2,47 +2,64 @@ require 'spec_helper'
 
 describe Zebra::SessionsController do
 
-  describe "without a valid token" do
-    it "#index" do
+  before(:all) do
+    @ticket = a_saved Ticket
+    @ticket.kind = Ticket::Kind::CREW
+    @ticket.save
+    @ticket.interactions.create(:key => 'wristband', :value => '123456')
+  end
+
+  describe "#index" do
+    it "with a valid cookie" do
+      request.cookies['zebra_token'] = "123456"
+      get :index
+      expect(response).to be_redirect
+    end
+
+    it "without a valid cookie" do
       get :index
       expect(response).to be_success
     end
-    
-    it "#new" do
-      get :show, :id => 1
+  end
+  
+  describe "#create" do
+    it "with a valid wristband id" do
+      post :create, {:wristband => '123456'}
+      expect(response.cookies['zebra_token']).to eq('123456')
+      expect(response).to be_redirect
+    end
+
+    it "without a valid wristband id" do
+      post :create, {:wristband => '654321'}
+      expect(response.cookies['zebra_token']).to eq(nil)
       expect(response).to be_redirect
     end
   end
 
-  describe "with a valid token" do
+  describe "#check_cookie" do
     
     before(:all) do
+      @controller = Zebra::SessionsController.new
+    end
+    
+    it "if wristband_id is nil" do
+      @controller.instance_eval{ check_cookie(nil) }.should eq(false)
+    end
+    
+    it "if wristband_id is not found" do
+      @controller.instance_eval{ check_cookie('654321') }.should eq(false)
+    end
+    
+    it "if wristband_id is not a crew ticket type" do
       ticket = a_saved Ticket
-      ticket.kind = Ticket::Kind::CREW
       ticket.save
-      ticket.interactions.create(:key => 'wristband', :value => '123456')
+      ticket.interactions.create(:key => 'wristband', :value => '234567')
+      @controller.instance_eval{ check_cookie('234567') }.should eq(false)
     end
     
-    it "#new" do
-      request.cookies['zebra_token'] = "123456"
-      get :show, :id => 1
-      expect(response).to be_success
+    it "if wristband_id is found and is a crew ticket type" do
+      @controller.instance_eval{ check_cookie('123456') }.should eq(true)
     end
-  end
-
-  describe "with an invalid token" do
-
-    before(:all) do
-      ticket = a_saved Ticket
-      ticket.interactions.create(:key => 'wristband', :value => '654321')
-    end
-
-    it "#new" do
-      request.cookies['zebra_token'] = "654321"
-      get :show, :id => 1
-      expect(response).to be_redirect
-    end
-
   end
 
 end
