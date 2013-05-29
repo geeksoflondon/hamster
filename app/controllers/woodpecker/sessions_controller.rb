992abefb614b07526b68fab37f3b21a4b0dad419
Kevin Prince <nextgengames@gmail.com>
2013-05-29 21:20:38 +0100

    Login logic for woodpecker.

---
+++ app/controllers/woodpecker/sessions_controller.rb
@@ -0,0 +1,47 @@
+#OnSite Registration Tool
+class Woodpecker::SessionsController < ApplicationController
+
+  before_filter :logged_in?, :except => [:index, :create]
+
+  layout 'woodpecker'
+
+  def index
+    puts check_cookie(cookies[:woodpecker_token]).inspect
+    redirect_to :woodpecker_confirm if check_cookie(cookies[:woodpecker_token])
+  end
+
+  def create
+    if check_cookie(params[:woodpecker_password]) === true
+      cookies[:woodpecker_token] = params[:woodpecker_password]
+      redirect_to :woodpecker_confirm
+    else
+      redirect_to :woodpecker_root
+    end
+  end
+
+  def destroy
+    cookies.delete :woodpecker_token
+    redirect_to :woodpecker_root
+  end
+
+  private
+  
+  def logged_in?
+    unless check_cookie(cookies[:woodpecker_token])
+      redirect_to :woodpecker_sessions
+    end
+  end
+
+  def check_token(token)
+    woodpecker_token = Interaction.where(:key => 'woodpecker_password', :value => token, :current => true)
+    woodpecker_token.empty? ? nil : woodpecker_token.first.interactable
+  end
+
+  def check_cookie(token = nil)
+    return false if token.nil?
+    woodpecker_token = check_token(token)
+    return false unless woodpecker_token.present?
+    true
+  end
+
+end
---
+++ spec/controllers/woodpecker/sessions_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe Woodpecker::SessionsController do
+
+  before(:all) do
+    @ticket = a_saved Ticket
+  end
+
+  describe "#index" do
+    it "with a valid cookie" do
+      request.cookies['woodpecker_token'] = @ticket.woodpecker_password
+      get :index
+      expect(response).to be_redirect
+    end
+
+    it "without a valid cookie" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+  
+  describe "#create" do
+    it "with a valid woodpecker password" do
+      post :create, {:woodpecker_password => @ticket.woodpecker_password}
+      expect(response.cookies['woodpecker_token']).to eq(@ticket.woodpecker_password)
+      expect(response).to be_redirect
+    end
+
+    it "without a valid woodpecker password" do
+      post :create, {:woodpecker_password => '654321'}
+      expect(response.cookies['woodpecker_token']).to eq(nil)
+      expect(response).to be_redirect
+    end
+  end
+
+  describe "#create via /woodpecker/onetime/:woodpecker_token" do
+    it "with a valid woodpecker password" do
+      get :woodpecker_onetime, {:woodpecker_password => @ticket.woodpecker_password}
+      expect(response.cookies['woodpecker_token']).to eq(@ticket.woodpecker_password)
+      expect(response).to be_redirect
+    end
+
+    it "without a valid woodpecker password" do
+      get :woodpecker_onetime, {:woodpecker_password => 'abcdefg'}
+      expect(response.cookies['woodpecker_token']).to eq(nil)
+      expect(response).to be_redirect
+    end
+  end
+
+  describe "#check_cookie" do
+    before(:all) do
+      @controller = Woodpecker::SessionsController.new
+    end
+    
+    it "if woodpecker_password is nil" do
+      @controller.instance_eval{ check_cookie(nil) }.should eq(false)
+    end
+    
+    it "if woodpecker_password is not found" do
+      @controller.instance_eval{ check_cookie('654321') }.should eq(false)
+    end
+
+    it "if woodpecker_password is found" do
+      woodpecker_password =  @ticket.woodpecker_password
+      @controller.instance_eval{ check_cookie(woodpecker_password) }.should eq(true)
+    end
+  end
+
+end
