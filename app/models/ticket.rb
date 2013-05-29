696672c8f0717693a8758a457f12d8bcdaacf2a6
Kevin Prince <nextgengames@gmail.com>
2013-05-29 18:54:49 +0100

    Implementation of the woodpecker password generation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -11,9 +11,23 @@ class Ticket < ActiveRecord::Base
 
   before_validation :set_kind
 
+  after_create :generate_woodpecker_password
+
   def set_kind
     self.kind ||= Ticket::Kind::REGULAR
     true
+  end
+  
+  def woodpecker_password
+    self.interactions.where(:key => 'woodpecker_password')
+  end
+  
+  private
+  
+  def generate_woodpecker_password
+    o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
+    token = Digest::SHA1.hexdigest("#{self.id}#{(0...50).map{ o[rand(o.length)] }.join}#{Time.now()}")
+    self.interactions.create :key => 'woodpecker_password', :value => token
   end
 
 end
