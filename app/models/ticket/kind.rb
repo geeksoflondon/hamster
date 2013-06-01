9efaed583b47daf10d82f8aa20b43fe1c4c0e393
Kevin Prince <nextgengames@gmail.com>
2013-06-01 14:23:29 +0100

    Updated kind so that we have a manger and rank ordered them

--- app/models/ticket/kind.rb
+++ app/models/ticket/kind.rb
@@ -1,10 +1,10 @@
 class Ticket
   class Kind
     REGULAR = 1.freeze
-    CREW =    2.freeze
+    SPONSOR = 2.freeze
     VIP =     3.freeze
-    VENUE =   4.freeze
-    SPONSOR = 5.freeze
+    CREW =    4.freeze
+    MANAGER = 5.freeze
 
     TYPES = (1..5).freeze
   end
