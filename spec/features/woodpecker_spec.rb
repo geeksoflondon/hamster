35cb79e5f0ee92ebfe29fc38fe189bfd70d57249
Melinda Seckington <mseckington@gmail.com>
2013-06-10 23:19:14 +0100

    Spec fixing

--- spec/features/woodpecker_spec.rb
+++ spec/features/woodpecker_spec.rb
@@ -10,22 +10,22 @@ describe "the signup process", :type => :feature do
   it "i confirm my ticket" do
     visit "/confirmations/#{@retain_token}/edit"
     find("#attending").set "true"
-    click_on "I am attending!!"
-    page.should have_content "Check whats on your badge"
+    click_on "I am attending!"
+    page.should have_content "Check what's on your badge"
     click_on "Next..."
     click_on "Save my details"
     @ticket.reload
     @ticket.is_confirmed?.should == true
     @ticket.is_attending?.should == true
   end
-  
+
   it "i cancel my ticket" do
     visit "/confirmations/#{@retain_token}/edit"
     find("#attending").set "false"
-    page.should have_content "Hello #{@ticket.attendee.first_name}  #{@ticket.attendee.last_name}"
+    page.should have_content "Hello, #{@ticket.attendee.first_name}  #{@ticket.attendee.last_name}"
     click_on "Cancel my ticket!"
-    page.should have_content "#{@ticket.attendee.first_name}, you sure you want to cancel?"
-    click_on "Yes cancel my ticket"
+    page.should have_content "#{@ticket.attendee.first_name}, are you sure you want to cancel?"
+    click_on "Yes, cancel my ticket."
     @ticket.reload
     @ticket.is_confirmed?.should == true
     @ticket.is_attending?.should == false
