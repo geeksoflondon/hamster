3dca77e6257f0048593a8afda1e3f957989d3770
Kevin Prince <nextgengames@gmail.com>
2013-06-29 18:22:19 +0100

    Fixed tickets so that you can only view tickets in that event.

--- app/controllers/zebra/ticket_controller.rb
+++ app/controllers/zebra/ticket_controller.rb
@@ -1,9 +1,9 @@
 class Zebra::TicketController < Zebra::SessionsController
 
   before_filter :logged_in?, :user, :event
-
+  before_filter :ticket, :except => [:new, :create]
+  
   def show
-    @ticket = Ticket.find(params[:id])
     @attendee = @ticket.attendee
     @interactions = @ticket.interactions.order('created_at DESC')
     @checkins = Interaction.unscoped.where({key: 'onsite', interactable_type: 'Ticket', interactable_id: @ticket.id}).order('created_at DESC')
@@ -14,7 +14,6 @@ class Zebra::TicketController < Zebra::SessionsController
   end
 
   def update
-    ticket = Ticket.find(params[:id])
     attendee = ticket.attendee
     attendee.first_name = params[:first_name]
     attendee.last_name = params[:last_name]
@@ -22,8 +21,8 @@ class Zebra::TicketController < Zebra::SessionsController
     attendee.notes = params[:notes]
     attendee.save
 
-    ticket.kind = params[:kind]
-    ticket.save
+    @ticket.kind = params[:kind]
+    @ticket.save
     flash[:notice] = "#{attendee.name} is now updated"
     redirect_to :zebra_ticket
   end
@@ -54,17 +53,26 @@ class Zebra::TicketController < Zebra::SessionsController
   end
 
   def checkin
-    ticket = Ticket.find(params[:ticket_id])
-    ticket.is 'onsite'
-    flash[:notice] = "#{ticket.attendee.name} is now checked in"
-    redirect_to "/zebra/ticket/#{ticket.id}"
+    @ticket.is 'onsite'
+    flash[:notice] = "#{@ticket.attendee.name} is now checked in"
+    redirect_to "/zebra/ticket/#{@ticket.id}"
   end
 
   def checkout
-    ticket = Ticket.find(params[:ticket_id])
-    ticket.isnt 'onsite'
-    flash[:notice] = "#{ticket.attendee.name} is now checked out"
-    redirect_to "/zebra/ticket/#{ticket.id}"
+    @ticket.isnt 'onsite'
+    flash[:notice] = "#{@ticket.attendee.name} is now checked out"
+    redirect_to "/zebra/ticket/#{@ticket.id}"
+  end
+  
+  private
+  
+  def ticket
+    params[:id] = params[:ticket_id] if params[:id].nil?
+    @ticket = Ticket.find(params[:id])
+    unless @ticket.event_id == @event.id
+      flash[:error] = "Sorry that ticket does not belong to this event"
+      redirect_to "/zebra/dashboard"
+    end
   end
 
 end
