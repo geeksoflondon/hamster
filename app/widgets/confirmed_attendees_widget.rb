b3ba45dce177d5a9d34126935a37745d74572f30
Kevin Prince <nextgengames@gmail.com>
2013-07-10 21:38:18 +0100

    Fixed wrong count

--- app/widgets/confirmed_attendees_widget.rb
+++ app/widgets/confirmed_attendees_widget.rb
@@ -4,7 +4,7 @@ widget :confirmed_attendees do
   event = Event.find(2)
   data do
     {
-      :value => event.attending_tickets_ids.count,
+      :value => event.confirmed_tickets_ids.count,
     }
   end
 end
