3126fa149a7a78c0b8c30641f267a6a51e0a71a7
Kevin Prince <nextgengames@gmail.com>
2013-07-10 21:50:34 +0100

    Fixed cancelled counter

--- app/widgets/cancelled_attendees_widget.rb
+++ app/widgets/cancelled_attendees_widget.rb
@@ -1,9 +1,10 @@
 widget :cancelled_attendees do
   key "613933a628c08495ce6132f1b5b434ff68021e4b"
   type "number_and_secondary"
+  event = Event.find(2)
   data do
     {
-      :value => Interaction.where(:key => 'attending', :value => 'false', :current => 'true').count(),
+      :value => event.not_attending_tickets_ids.count,
     }
   end
 end
