75d4bcd986cfed5010fbe38de5ec1b9ff4e66364
Kevin Prince <nextgengames@gmail.com>
2013-07-14 16:23:17 +0100

    arrival number was not needed in the onsite widget so removed.

--- app/widgets/onsite_widget.rb
+++ app/widgets/onsite_widget.rb
@@ -2,7 +2,6 @@ widget :onsite do
   key "ee3322dbeb1f9fe07b2a224c1b4d7e3e35aae7aa"
   type "geckometer"
   event = Event.find(2)
-  arrived = event.arrived_tickets_ids.count
   onsite = event.onsite_tickets_ids.count
   data do
     {
