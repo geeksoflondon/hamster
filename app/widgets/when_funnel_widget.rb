26ec8dce471eb6e1c63b609a6c82b4b4abdcfbab
Kevin Prince <nextgengames@gmail.com>
2013-07-14 16:02:55 +0100

    Tweaking the when widget to correctly display data.

--- app/widgets/when_funnel_widget.rb
+++ app/widgets/when_funnel_widget.rb
@@ -2,8 +2,6 @@ widget :when_funnel do
   key "3f26b21a9cf42a7dcfaab94f602aca30f62dc020"
   type "funnel"
   event = Event.find(2)
-  total = event.tickets.count
-  confirmed = event.confirmed_tickets_ids.count
   saturday = event.attending_saturday_tickets_ids.count
   sunday = event.attending_sunday_tickets_ids.count
   overnight = event.attending_overnight_tickets_ids.count
@@ -11,7 +9,6 @@ widget :when_funnel do
     {
       :hide_percentage => true,
       :items => [
-        {:value => confirmed, :label => 'Confirmed Attending'},
         {:value => saturday, :label => 'Saturday'},
         {:value => overnight, :label => 'Overnight'},
         {:value => sunday, :label => 'Sunday'}
